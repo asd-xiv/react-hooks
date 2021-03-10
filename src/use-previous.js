f1b2380fe7001d8bcbbd942fc77491c4888007f0
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-03-10 13:28:19 +0100

    docs: Add example

--- src/use-previous.js
+++ src/use-previous.js
@@ -4,6 +4,12 @@ import { useRef } from "react"
 
 import { useEffect } from "./use-deep"
 
+/**
+ *
+ * @example
+ *
+ * const prevId = usePrev(id)
+ */
 export const usePrevious = value => {
   // The ref object is a generic container whose current property is mutable
   // and can hold any value, similar to an instance property on a class.
