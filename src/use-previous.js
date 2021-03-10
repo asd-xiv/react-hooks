3c1f9dc0122c7282a866d7efe6c732f297174b95
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-03-10 13:44:11 +0100

    chore: Fix linting errors

--- src/use-previous.js
+++ src/use-previous.js
@@ -5,20 +5,22 @@ import { useRef } from "react"
 import { useEffect } from "./use-deep"
 
 /**
+ * @param {any} source
+ *
+ * @returns {any}
  *
  * @example
- *
  * const prevId = usePrev(id)
  */
-export const usePrevious = value => {
+export const usePrevious = source => {
   // The ref object is a generic container whose current property is mutable
   // and can hold any value, similar to an instance property on a class.
   const reference = useRef()
 
   // Store current value in ref
   useEffect(() => {
-    reference.current = value
-  }, [value])
+    reference.current = source
+  }, [source])
 
   // Return previous value (happens before update in useEffect above)
   return reference.current
